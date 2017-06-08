323c8fd5f1c6e3237e346bcb0744f61a38a89a9c
Jonathan Channon <jonathan.channon@gmail.com>
2017-06-08 18:44:09 +0100

    strongnaming

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace strongnaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = ConfigurationOptions.Parse("");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
